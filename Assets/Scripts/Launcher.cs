085fc0004ffa18ed7670eefbb8b1297b396a0d9d
rvanderaspoilden <rodolphevanderaspoilden@gmail.com>
2020-03-14 12:13:23 +0100

    chores: add photon

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master server");
+    }
+    
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+}
